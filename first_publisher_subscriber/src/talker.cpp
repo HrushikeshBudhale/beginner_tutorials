6a307d69dea90fa0ee22aa3bd1b1e9106376fcf6
Hrushikesh <hrushikesh@flytbase.com>
2021-11-06 21:09:08 -0400

    Added new service in talker node

--- first_publisher_subscriber/src/talker.cpp
+++ first_publisher_subscriber/src/talker.cpp
@@ -12,12 +12,27 @@
 #include <sstream>
 #include "ros/ros.h"
 #include "std_msgs/String.h"
+#include "first_publisher_subscriber/modify_str.h"
+
+std_msgs::String log_msg;
+
+bool modify_output(first_publisher_subscriber::modify_str::Request &req,
+                   first_publisher_subscriber::modify_str::Response &res) {
+  ROS_DEBUG_STREAM("[Talker] Received req: " << req.data);
+  log_msg.data = req.data;
+  res.new_data = "Publish message updated";
+  res.status = true;
+  ROS_DEBUG_STREAM("[Talker] Responding with resp: " << res.new_data);
+  return true;
+}
 
 
 int main(int argc, char **argv) {
   ros::init(argc, argv, "talker");
   ros::NodeHandle n;
   ros::Publisher chatter_pub = n.advertise<std_msgs::String>("chatter", 1000);
+  ros::ServiceServer service = n.advertiseService("modify_output",
+                                                    modify_output);
 
   ros::Rate loop_rate(10);
 
